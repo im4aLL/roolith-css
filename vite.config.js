c15ba7f03f2cc8224e29ec485628a75ea6cf6d51
im4aLL <me@habibhadi.com>
2025-07-30 22:05:06 -0600

    changed tooling to vite

---
+++ vite.config.js
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite';
+import pkg from './package.json';
+import banner from 'vite-plugin-banner';
+import { resolve } from 'path';
+
+const bannerContent = `/**\n * Name: ${pkg.name}\n * Version: v${pkg.version}\n * Description: ${
+    pkg.description
+}\n * Author: ${pkg.author}\n * Generated on ${new Date()}\n */`;
+
+export default defineConfig({
+    plugins: [banner(bannerContent)],
+    build: {
+        rollupOptions: {
+            input: {
+                roolith: resolve(__dirname, 'src/sass/roolith.scss'),
+                'roolith-dark': resolve(__dirname, 'src/sass/roolith-dark.scss'),
+            },
+            output: {
+                assetFileNames: `[name]-${pkg.version}[extname]`,
+            },
+        },
+        outDir: 'dist',
+        emptyOutDir: true,
+    },
+});
